453a4f4763481c94fb375917a9f2493054110af5
Frederick Pearson <fredp613@gmail.com>
2017-06-02 16:31:53 -0400

    fiscal year

--- Models/Helpers/FiscalYear.cs
+++ Models/Helpers/FiscalYear.cs
@@ -8,9 +8,28 @@ namespace InternalPortal.Models.Helpers
     public class FiscalYear
     {
         public DateTime DateToProcess { get; set; }
-        public string GetFiscalYear()
+        public static string GetFiscalYearByDateTime(DateTime dateToProcess)
         {
-            throw new NotImplementedException();
+            FiscalYear fy = new FiscalYear
+            {
+                DateToProcess = dateToProcess
+            };
+            
+            var year = dateToProcess.Year;
+            var month = dateToProcess.Month;
+            var day = dateToProcess.Day;
+
+            if (month >= 4 && month <= 12)
+            {
+                return year.ToString() + (year + 1).ToString();
+            }
+            if (month >= 1 && month <= 3)
+            {
+                return (year - 1).ToString() + year.ToString();
+            }
+
+            return null;
+
         }
     }
 }
