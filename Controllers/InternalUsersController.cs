112c1c4ca953dbc068062880e9925d9b8430ce19
Pearson <FPearson@justice.gc.ca>
2017-08-16 22:53:55 -0400

    t

--- Controllers/InternalUsersController.cs
+++ Controllers/InternalUsersController.cs
@@ -67,7 +67,7 @@ namespace InternalPortal.Controllers
                                            .Include(iu => iu.FundingProgramInternalUsers)
                                                  .ThenInclude(fp => fp.FundingProgram)
                                             .SingleOrDefault(m => m.UserName == username);
-            var internalUserRoles = _context.InternalUserRole.Where(iur => iur.InternalUserId == internalUser.InternalUserId).Select(u => u.Role).ToString();
+            var internalUserRoles = _context.InternalUserRole.Where(iur => iur.InternalUserId == internalUser.InternalUserId).Select(u => u.Role).ToList().ToString();
             if (internalUserRoles != null)
             {
                 Debug.WriteLine(internalUserRoles.ToString());
