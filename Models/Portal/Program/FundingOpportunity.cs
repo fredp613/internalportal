1ef9d91679c86e523b1ebcde3be306ecaff9cd3d
Frederick Pearson <fredp613@gmail.com>
2017-10-02 14:08:43 -0400

    t

--- Models/Portal/Program/FundingOpportunity.cs
+++ Models/Portal/Program/FundingOpportunity.cs
@@ -24,6 +24,7 @@ namespace InternalPortal.Models.Portal.Program
         [Key]
         public Guid FundingOpportunityId { get; set; }
         public Guid FundingProgramId { get; set; }
+        [JsonIgnore]
         public FundingProgram FundingProgram { get; set; }       
         public string TitleE { get; set; }
         public string TitleF { get; set; }
