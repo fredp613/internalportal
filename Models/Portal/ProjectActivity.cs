89b3064027cc5793215591d2a145893440ed28b9
Pearson <FPearson@justice.gc.ca>
2017-11-17 11:45:04 -0500

    t

--- Models/Portal/ProjectActivity.cs
+++ Models/Portal/ProjectActivity.cs
@@ -15,6 +15,7 @@ namespace InternalPortal.Models.Portal
         [JsonIgnore]
         public Project Project { get; set; }
         public string Title { get; set; }
+        public string ResponsibleParty { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Output { get; set; }
