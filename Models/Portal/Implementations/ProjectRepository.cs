21fd50ba6df4a859862c61eff94d3deaebfdda25
Pearson <FPearson@justice.gc.ca>
2017-11-26 08:59:18 -0500

    t

--- Models/Portal/Implementations/ProjectRepository.cs
+++ Models/Portal/Implementations/ProjectRepository.cs
@@ -40,11 +40,11 @@ namespace InternalPortal.Models.Portal.Implementations
         public IEnumerable<Project> GetWorkloadManagerSubmittedProjects()
         {
             var projs = PortalContext.Project.Where(p => p.ProjectStatus == Status.Submitted);
-            foreach (var proj in projs)
-            {
-                proj.ContactName = PortalContext.Contact.SingleOrDefault(c => c.ContactId == proj.ContactId).FullName;
-                proj.FundingOpportunityName = PortalContext.FundingOpportunity.SingleOrDefault(f => f.FundingOpportunityId == proj.FundingOpportunityID).TitleE;
-            }
+            //foreach (var proj in projs)
+            //{
+            //    proj.ContactName = PortalContext.Contact.SingleOrDefault(c => c.ContactId == proj.ContactId).FullName;
+            //    proj.FundingOpportunityName = PortalContext.FundingOpportunity.SingleOrDefault(f => f.FundingOpportunityId == proj.FundingOpportunityID).TitleE;
+            //}
             return projs;
         }
 
